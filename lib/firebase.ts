f313557ad62bcea3b6f5b1adc10b363c40266e1e
Fabri1402 <fabri1402flores@gmail.com>
2026-06-26 09:44:46 -0500

    Añadidos Firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyA3c24KtBxxndxHCjT9rIvvme64ABpYqi4",
+  authDomain: "tecemtf.firebaseapp.com",
+  projectId: "tecemtf",
+  storageBucket: "tecemtf.firebasestorage.app",
+  messagingSenderId: "981549478614",
+  appId: "1:981549478614:web:8c7075a5b8c1082a790f77",
+  measurementId: "G-HDSY751DFV"
 };
 
 const app = initializeApp(firebaseConfig);
